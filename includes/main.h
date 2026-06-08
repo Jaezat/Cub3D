6f72b89ccf3da4716a4275eea3faa97a91fde3a8
leschunc <leschunc@student.42lisboa.com>
2026-06-08 16:12:42 +0100

    Merged structures from past commit with full changes (#21)
    Merged structures from past commit with full changes
    
    ---------
    
    Co-authored-by: Jazmin Acosta <163866069+Jaezat@users.noreply.github.com>

--- includes/main.h
+++ includes/main.h
@@ -6,19 +6,16 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
-# include <sys/time.h>
 # include <math.h>
-# include <X11/X.h>
-# include <X11/keysym.h>
+# include <sys/time.h>
+
+# define _USE_MATH_DEFINES
 
 # include "../mlx_linux/mlx.h"
-
-# include "structs.h"
-
 # include "parser/p_structs.h"
+# include "wdw_mlx_struct.h"
 # include "parser/p_functions.h"
 # include "parser/p_utils.h"
-# include "raycast/raycast.h"
 # include "minimap.h"
 
 # define WIN_W 800
@@ -26,11 +23,21 @@
 # define TO_RATIO / 1
 # define WIN_H WIN_W TO_RATIO
 
-# define M_PI 3.14159265358979323846
 # define RAD M_PI / 180.0
 
-#define MOV_INC 0.1
-#define ANG_INC 2
+# define K_LEFT 65361
+# define K_UP 65362
+# define K_RIGHT 65363
+# define K_DOWN 65364
+# define K_ESC 65307
+
+# define K_A 97
+# define K_W 119
+# define K_D 100
+# define K_S 115
+
+#define MOV_INC 0.25
+#define ANG_INC 6
 
 // keep this ? ////////////////////////////////////////////////////////////////
 typedef int				(*t_clean)();
